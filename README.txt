eb56c21aa446c99a6a5bef9dc5152e74f75abfb3
hshan <hhsung0120@naver.com>
2022-02-08 00:32:05 +0900

    배치 공부

--- README.txt
+++ README.txt
@@ -17,6 +17,9 @@ batch_job_execution_params
 batch_job_execution_seq
 
 batch_job_instance
+    * JOB_INSTANCE_ID : PK
+    * JOB_NAME : 수행한 배치의 JOB 이름
+    * JOB PARAMETER 에 따라 데이터 생성
 
 batch_job_seq
 
@@ -25,3 +28,28 @@ batch_step_execution
 batch_step_execution_context
 
 batch_step_execution_seq
+
+
+
+
+
+================================================================================================
+흐름 참고용
+https://jojoldu.tistory.com/328?category=902551
+@Bean
+    public Job stepNextConditionalJob() {
+        return jobBuilderFactory.get("stepNextConditionalJob")
+                .start(conditionalJobStep1())
+                    .on("FAILED") // FAILED 일 경우
+                    .to(conditionalJobStep3()) // step3으로 이동한다.
+                    .on("*") // step3의 결과 관계 없이
+                    .end() // step3으로 이동하면 Flow가 종료한다.
+                .from(conditionalJobStep1()) // step1로부터
+                    .on("*") // FAILED 외에 모든 경우
+                    .to(conditionalJobStep2()) // step2로 이동한다.
+                    .next(conditionalJobStep3()) // step2가 정상 종료되면 step3으로 이동한다.
+                    .on("*") // step3의 결과 관계 없이
+                    .end() // step3으로 이동하면 Flow가 종료한다.
+                .end() // Job 종료
+                .build();
+================================================================================================
--- src/main/java/site/heeseong/batch/job/CreateArticleJobConfig.java
+++ src/main/java/site/heeseong/batch/job/CreateArticleJobConfig.java
@@ -1,19 +1,20 @@
-package site.heeseong.batch.jobconfig;
+package site.heeseong.batch.job;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -33,27 +34,41 @@ public class CreateArticleJobConfig {
     @Bean
     public Job createArticleJob() {
         return jobBuilderFactory.get("createArticleJob")
-                .incrementer(new RunIdIncrementer())
-                .start(createArticleStep())
+                .start(createArticleStep(null))
+                .next(createArticleStep2(null))
                 .build();
     }
 
     @Bean
-    public Step createArticleStep() {
+    @JobScope
+    public Step createArticleStep(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("createArticleStep")
-                .<ArticleModel, ArticleEntity>chunk(100)
-                .reader(createArticleReader())
-                .processor(createArticleProcessor())
-                .writer(createArticleWriter())
-//                .faultTolerant()
-//                .skip(FlatFileParseException.class)
-//                .skipLimit(10)
-                .build();
-//                .tasklet((contribution, chunkContext) -> {
-//                    log.info("step1");
-//                    return RepeatStatus.FINISHED;
-//                })
+//                .<ArticleModel, ArticleEntity>chunk(100)
+//                .reader(createArticleReader())
+//                .processor(createArticleProcessor())
+//                .writer(createArticleWriter())
+////                .faultTolerant()
+////                .skip(FlatFileParseException.class)
+////                .skipLimit(10)
 //                .build();
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step1");
+                    log.info("step1 data {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step createArticleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("createArticleStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("step2");
+                    log.info("step2 data {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
     }
 
     @Bean
